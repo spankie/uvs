4b71774ea5f8bfdecd2f1ba22670c4373999578a
spankie <odohi.david@gmail.com>
2017-03-18 00:31:00 +0100

    enhancements and admin login

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var { data } = require('../models/data');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('register', data);
 });
 
 module.exports = router;
