a65e435af1067891d914406d8678079a14917f02
spankie <odohi.david@gmail.com>
2017-03-24 16:13:46 +0100

    admin login plus dashboard UI enhancement

--- models/db_config.js
+++ models/db_config.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 module.exports.pool = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'toor',
     database: 'uvs'
