96b3eabebdaee04c1055cc9126b3caef1520dad1
spankie <odohi.david@gmail.com>
2017-04-22 18:18:07 +0100

    Fixed small variable clash bug, from admin to landing page...

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var { data } = require('../models/data');
+// var { data } = require('../models/data');
+// this is to eliminate other objects added to data from other routes...
+myData = {
+    title: "Leap Development Knights"
+}
 var { pool } = require('../models/db_config')
 
+// var myData = data;
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // check if there is an election today. //
@@ -10,13 +15,15 @@ router.get('/', function(req, res, next) {
         connection.query("SELECT id FROM elections WHERE DATE(date) = DATE(NOW())", function(err, results, fields) {
             if (err) {
                 console.log(err);
-                res.render('index', data);
+                res.render('index', myData);
             } else {
                 if(results.length > 0) {
-                    data.election = results[0].id;
-                    res.render('index', data);
+                    myData.election = results[0].id;
+                    console.log("ELECTION :: ", myData.election);
+                    res.render('index', myData);
                 } else {
-                    res.render('index', data);
+                    // console.log("ELECTION2 :: ", data.election, data.candidates);
+                    res.render('index', myData);
                 }
             }
             connection.release();
